973a601a7ee8851f3bf3f79f410ccdaaeed3ff25
Renat <riko321riko321@gmail.com>
2023-09-07 15:59:21 +0500

    реалзиованы тесты assert-rules

---
+++ tests/domain/validator/field/rules/assert-rules-test/cannot-be-empty-array.a-rule.test.ts
@@ -0,0 +1,26 @@
+import { CannotBeEmptyArrayAssertionRule } from "../../../../../../src/domain/validator/rules/assert-rules/cannot-be-empty-array.a-rule";
+
+describe('can not empty array rule tests', () => {
+    test('success, received value equal array', () => {
+      const sut = new CannotBeEmptyArrayAssertionRule();
+      const result = sut.validate([1]);
+      expect(result).toEqual({
+        behaviour: 'RunNextRule',
+      });
+    });
+
+    test('fail, received value equal empty array', () => {
+     const sut = new CannotBeEmptyArrayAssertionRule();
+     const result = sut.validate([]);
+     expect(result).toEqual({
+        behaviour: 'SaveErrorAndBreakValidation',
+        ruleError: {
+          "hint": {},
+          "text": "Значение должно быть не пустым массивом данных",
+        }
+      });
+    });
+
+       
+
+  });
