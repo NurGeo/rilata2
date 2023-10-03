0133c21ec4d32c621769eaef1256499c930be2ef
KLeBer_Lacky <126053472+KLeBerLacky@users.noreply.github.com>
2023-10-03 16:13:46 +0500

    Update positive-number.v-rule.ts

--- src/domain/validator/rules/validate-rules/number/positive-number.v-rule.ts
+++ src/domain/validator/rules/validate-rules/number/positive-number.v-rule.ts
@@ -2,15 +2,11 @@ import { ValidationRuleAnswer } from '../../types';
 import { ValidationRule } from '../../validation-rule';
 
 export class PositiveNumberValidationRule extends ValidationRule<'validate', number> {
-  requirement = 'Число должно быть положительным {{number}}';
-
-  constructor(private positiveNumber: number = 0) {
-    super();
-  }
+  requirement = 'Число должно быть положительным';
 
   validate(value: number): ValidationRuleAnswer {
-    return value >= this.positiveNumber
+    return value >= 0
       ? this.returnSuccess('RunNextRule')
-      : this.returnFail('SaveErrorAndRunNextRule', { number: this.positiveNumber });
+      : this.returnFail('SaveErrorAndRunNextRule');
   }
 }
