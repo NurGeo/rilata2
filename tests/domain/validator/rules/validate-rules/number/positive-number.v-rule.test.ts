3270e4004589defd00f79bb9c69b53f64186685a
KLeBer_Lacky <126053472+KLeBerLacky@users.noreply.github.com>
2023-10-03 16:10:53 +0500

    Update positive-number.v-rule.test.ts

--- tests/domain/validator/rules/validate-rules/number/positive-number.v-rule.test.ts
+++ tests/domain/validator/rules/validate-rules/number/positive-number.v-rule.test.ts
@@ -2,28 +2,28 @@ import { describe, expect, test } from 'bun:test';
 import { PositiveNumberValidationRule } from '../../../../../../src/domain/validator/rules/validate-rules/number/positive-number.v-rule';
 
 describe('Number must be positive', () => {
-  test('success, the resulting value is positive', () => {
     const sut = new PositiveNumberValidationRule();
+  test('success, the resulting value is positive', () => {
     const result = sut.validate(10);
     expect(result).toEqual({ behaviour: 'RunNextRule' });
   });
+
   test('success, resulting value is 0', () => {
-    const sut = new PositiveNumberValidationRule();
     const result = sut.validate(0);
     expect(result).toEqual({ behaviour: 'RunNextRule' });
   });
+
   test('success, resulting value is infinity', () => {
-    const sut = new PositiveNumberValidationRule();
     const result = sut.validate(Infinity);
     expect(result).toEqual({ behaviour: 'RunNextRule' });
   });
+
   test('success, resulting value is -0', () => {
-    const sut = new PositiveNumberValidationRule();
     const result = sut.validate(-0);
     expect(result).toEqual({ behaviour: 'RunNextRule' });
   });
+
   test('failure, resulting value is a negative number', () => {
-    const sut = new PositiveNumberValidationRule();
     const result = sut.validate(-5);
     expect(result).toEqual({
       behaviour: 'SaveErrorAndRunNextRule',
@@ -31,12 +31,12 @@ describe('Number must be positive', () => {
         hint: {
           number: 0,
         },
-        text: 'Число должно быть положительным {{number}}',
+        text: 'Число должно быть положительным',
       },
     });
   });
+
   test('failure, resulting value is NaN', () => {
-    const sut = new PositiveNumberValidationRule();
     const result = sut.validate(NaN);
     expect(result).toEqual({
       behaviour: 'SaveErrorAndRunNextRule',
@@ -44,12 +44,12 @@ describe('Number must be positive', () => {
         hint: {
           number: 0,
         },
-        text: 'Число должно быть положительным {{number}}',
+        text: 'Число должно быть положительным',
       },
     });
   });
+
   test('failure, resulting value is a negative Infinity', () => {
-    const sut = new PositiveNumberValidationRule();
     const result = sut.validate(-Infinity);
     expect(result).toEqual({
       behaviour: 'SaveErrorAndRunNextRule',
@@ -57,7 +57,7 @@ describe('Number must be positive', () => {
         hint: {
           number: 0,
         },
-        text: 'Число должно быть положительным {{number}}',
+        text: 'Число должно быть положительным',
       },
     });
   });
